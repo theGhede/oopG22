f90c41ca4d26f8f042f3f54057bed0c917f2ecce
Florian <the-ghede@web.de>
2018-10-16 11:54:46 +0200

    neighborhood distances

--- src/oopG22/Test.java
+++ src/oopG22/Test.java
@@ -1,16 +1,8 @@
 package oopG22;
 
 public class Test {
-
-	// Berechnen der euklidischen Distanz
-	public double distance (Bird a, Bird b) {
-		double dist;
-		double xdist = Math.pow((a.x - b.x), 2);
-		double ydist = Math.pow((a.y - b.y), 2);
-		return dist = Math.sqrt(xdist + ydist);
-	}
 	
-	// 
+	// Vogel - besteht aus Koordinaten und eine Nachbarschaft, errechnet aus den (5-20) nähesten anderen Vögeln
 	private class Bird {
 		double xcoord;
 		double ycoord;
@@ -21,23 +13,37 @@ public class Test {
 			for (int i = 0; i < 20; i++) {
 				neighbors[i] = -1;
 			}
+			
 			int amount = (int) (5 + (Math.random() * 15));
 			
 			for (int i = 0; i < amount; i++) {
-				
+				double[] distances = new double[199];
+				for (int j = 0; j < 200; j++) {
+					if (b.xcoord == flock[j].xcoord && b.ycoord == flock[j].ycoord) {
+						distances[j] = distance(b, flock[j]);
+					}
+				}
 			}
 			
 			return neighbors;
 		}
 	}
 	
+	// Der Schwarm wird von einem Array von Vögeln repräsentiert
+	// Die Anzahl der Vögel ist willkürlich vorbestimmt
+	public Bird[] flock = new Bird[199];
+	
+	// Berechnen der euklidischen Distanz
+	public double distance (Bird a, Bird b) {
+		double dist;
+		double xdist = Math.pow((a.xcoords - b.xcoords), 2);
+		double ydist = Math.pow((a.ycoords - b.ycoords), 2);
+		return dist = Math.sqrt(xdist + ydist);
+	}
 	
 	
 	public static void main(String[] args) {
 		
-		// Der Schwarm wird von einem Array von Vögeln repräsentiert
-		// Die Anzahl der Vögel ist willkürlich vorbestimmt
-		Bird[] flock = new Bird[200];
 		int amount = (int) (5 + (Math.random() * 15));
 		System.out.println("" + amount);
 	}
