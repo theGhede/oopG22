2c9bd6964d883e5de80ce91840ae4b0c574d04ad
Ignjat96 <44192760+Ignjat96@users.noreply.github.com>
2018-10-16 13:39:32 +0200

    Move

--- src/oopG22/Test.java
+++ src/oopG22/Test.java
@@ -29,33 +29,49 @@ public class Test {
 
 			return neighbors;
 		}
-		
-		void moveUp(int k) {
+
+		void moveUp(double k) {
 			double move = this.ycoord + k;
 			this.ycoord = move;
 		}
-		
-		void moveDown(int k) {
+
+		void moveDown(double k) {
 			double move = this.ycoord - k;
 			this.ycoord = move;
 		}
 
-		void moveLeft(int k) {
+		void moveLeft(double k) {
 			double move = this.xcoord - k;
 			this.xcoord = move;
 		}
 
-		void moveRight(int k) {
+		void moveRight(double k) {
 			double move = this.xcoord + k;
 			this.xcoord = move;
 		}
-		
+
+		// Nachbarschaftsbewegung, moveUp = y1, moveDown = y2, moveRight = x1, moveLeft = x2
+		//Im Flug können sich die Vögeln stärker annähern als die mindestens Distanz
+
+		void moveBird (Bird b, double x1,double x2, double y1, double y2){
+
+			if (!moved[b.index]){
+				moveUp(y1);
+				moveDown(y2);
+				moveLeft(x2);
+				moveRight(x1);
+			}
+			for (int i = 0; i < b.neighbors.length ; i++) {
+				moveBird(b.neighbors[i],x1,x2,y1,y2);
+			}
+
+		}
 	}
 
 	// Der Schwarm wird von einem Array von Vögeln repräsentiert
 	// Die Anzahl der Vögel ist willkürlich vorbestimmt
 	public Bird[] flock = new Bird[199];
-
+	public boolean[] moved = new boolean[199];
 
 	// Mindestabstand - willkürlich definiert
 	public double minDistance = 4;
@@ -68,8 +84,7 @@ public class Test {
 			}
 		}
 	}
-	
-	// Nachbarschaftsbewegung
+
 	
 	
 	// Berechnen der euklidischen Distanz
