c628b9e7ac9322e7f2a361a9025c8cb5d342f60e
Florian <the-ghede@web.de>
2018-10-16 10:47:08 +0200

    distance

--- src/oopG22/Test.java
+++ src/oopG22/Test.java
@@ -2,9 +2,16 @@ package oopG22;
 
 public class Test {
 
+	// Berechnen der euklidischen Distanz
+	public double distance (Bird a, Bird b) {
+		double dist;
+		double xdist = Math.pow((a.x - b.x), 2);
+		double ydist = Math.pow((a.y - b.y), 2);
+		return dist = Math.sqrt(xdist + ydist);
+	}
+		
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
-
 }
