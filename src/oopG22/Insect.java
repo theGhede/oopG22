39667d3194f17c065e2721ad99a0a1b708a011c6
Florian <the-ghede@web.de>
2018-10-23 22:10:44 +0200

    movement WIP

--- src/oopG22/Insect.java
+++ src/oopG22/Insect.java
@@ -2,5 +2,5 @@ package oopG22;
 
 // insects which can crawl over eachother and are tireless but react to danger if we want them to
 public class Insect extends Animal{
-	// TODO
+	boolean stressed;
 }
