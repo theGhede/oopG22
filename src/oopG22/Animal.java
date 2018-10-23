270569b762c03bfab7c74d1f8dc3c6240980dc8d
Florian <the-ghede@web.de>
2018-10-23 21:06:33 +0200

    quick fix for OOB exception

--- src/oopG22/Animal.java
+++ src/oopG22/Animal.java
@@ -12,6 +12,7 @@ public class Animal {
 	// this lets us manipulate the length of an animals movement
 	double modifier;
 
+	boolean existing = false;
 	// TODO: issue: movement currently cannot be diagonal & this is very apparent with animations in place
 	// TODO: fix existing
 	void moveUp(double k) throws InterruptedException {
--- src/oopG22/Insect.java
+++ src/oopG22/Insect.java
@@ -1,5 +1,6 @@
 package oopG22;
 
+// insects which can crawl over eachother and are tireless but react to danger if we want them to
 public class Insect extends Animal{
-
+	// TODO
 }
