0aab8e4685caf1b35b015c583423d62c8fe481aa
Ignjat96 <44192760+Ignjat96@users.noreply.github.com>
2018-11-13 23:21:12 +0100

    Asssertion

--- src/Aufgabe4/Animal.java
+++ src/Aufgabe4/Animal.java
@@ -1,8 +1,11 @@
 package Aufgabe4;
 
 public interface Animal {
-
+    
+    /* assertion { stundenAnzahl >= 0 } */
     int air();
+    /* assertion { stundenAnzahl >= 0 } */
     int water();
+    /* assertion { stundenAnzahl >= 0 } */
     int ground();
 }
