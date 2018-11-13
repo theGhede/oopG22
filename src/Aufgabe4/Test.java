4179f8df68d5cb8431cf39ee5da966d88177c748
Florian <the-ghede@web.de>
2018-11-14 00:13:39 +0100

    Subtyping WIP

--- src/Aufgabe4/Test.java
+++ src/Aufgabe4/Test.java
@@ -2,7 +2,7 @@ package Aufgabe4;
 
 public class Test {
 	/* Two new Interfaces:
-	 * 	- HerdMammal: new addition for Bird-FlightlessBird-HerdAnimal-PackAnimal-Mammal cluster
+	 * 	- HerdMammal:   new addition for Bird-FlightlessBird-HerdAnimal-PackAnimal-Mammal cluster
 	 * 					because some HerdAnimals are FliglessBirds (which all are Birds) and some HerdAnimals are Mammals
 	 * 					and all PackAnimals are HerdAnimals and Mammals - HerdMammal helps distinguish HerdAnimals (Bird/Mammal)
 	 * 	- ShoalingFish: without this either no or all Fish would have to be swarming
@@ -10,8 +10,15 @@ public class Test {
 	
 	/*
 	 * TODO:
-	 * nicht bestehende Untertypenbeziehungen
-	 * 		- 
+	 * nicht bestehende Untertypenbeziehungen:
+	 * 		- HerdAnimal & HerdMammal / FlightlessBird: Vererbung vorhanden, aber keine Untertypen, da getAlpha() einen return type
+	 * 													des ensprechenden Tiers hat (Ersetzbarkeit also nur möglich falls
+	 * 													HerdAnimal = HerdMammal [bzw. FlightlessBird] gilt)
+	 * 		- SocialAnimal & HerdAnimal:				"HerdAnimal getAlpha()" stört Ersetzbarkeit;
+	 * 													
+	 * 
+	 * 		- alle Typen welche nicht auf dem selben Vererbungs-Pfad liegen (z.B. MigratoryLocust & PackAnimal);
+	 * 		  da Untertypen Vererbung vorraussetzen
 	 */
 
 	public static void main(String[] args) {
