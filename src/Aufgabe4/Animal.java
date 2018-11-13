2bd3f6bea178a7d405937f1fa89084a3b6ea77ec
Florian <the-ghede@web.de>
2018-11-13 23:44:54 +0100

    additional animal assertion

--- src/Aufgabe4/Animal.java
+++ src/Aufgabe4/Animal.java
@@ -1,11 +1,15 @@
 package Aufgabe4;
 
 public interface Animal {
-    
-    /* assertion { stundenAnzahl >= 0 } */
-    int air();
-    /* assertion { stundenAnzahl >= 0 } */
-    int water();
-    /* assertion { stundenAnzahl >= 0 } */
-    int ground();
+
+	/* assertion { stundenAnzahl >= 0 } */
+	int air();
+
+	/* assertion { stundenAnzahl >= 0 } */
+	int water();
+
+	/* assertion { stundenAnzahl >= 0 } */
+	int ground();
+	
+	// Nachbedingung - min. einer der drei Erwartungswerte ist größer als 0
 }
