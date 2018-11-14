c7e7ff1d080c1e6a0df89c441bb4becf555f587d
Florian <the-ghede@web.de>
2018-11-14 10:06:43 +0100

    refining PackAnimal test case

--- src/Aufgabe4/Animal.java
+++ src/Aufgabe4/Animal.java
@@ -2,17 +2,22 @@ package Aufgabe4;
 
 public interface Animal {
 
-	// Postconditions for each method
+	// Server-controlled History Constraints for each method
+	/*
+	 * Note: Diese Zusicherung gilt auch für alle Untertypen von Animal, ist bei
+	 * 		 vielen Untertypen gleich aber bei anchen der Untertypen strenger (z.B. Fish).
+	 * 		 Dies ist für Ersetzbarkeit kein Problem, da es sich hier nicht um
+	 * 		 Vorbedingungen handelt (-> equal or less strict Precondition & guarantees
+	 * 		 same or more)
+	 */
 	int air();
-	/* assertion { stundenAnzahl >= 0 } */
-
+	/* assertion { expected time spent >= 0 } */
 
 	int water();
-	/* assertion { stundenAnzahl >= 0 } */
-
+	/* assertion { expected time spent >= 0 } */
 
 	int ground();
-	/* assertion { stundenAnzahl >= 0 } */
-	
+	/* assertion { expected time spent >= 0 } */
+
 	// Nachbedingung - min. einer der drei Erwartungswerte ist größer als 0
 }
