b498a30a4d022b6699ce82daca00956144d1c4d0
Florian <the-ghede@web.de>
2018-11-20 18:32:38 +0100

    Iterator basic methods

--- src/aufgabe5/FitAnimal.java
+++ src/aufgabe5/FitAnimal.java
@@ -1,56 +1,63 @@
 package aufgabe5;
- /*
+/*
 * TODO
 *     - Konstruktor
 *
- */
+*/
 
 public class FitAnimal {
 
-    private int fitness;
-    private boolean hierarchical;
+	private int fitness;
+	private boolean hierarchical;
 
+	public int getFitness() {
+		return this.fitness;
+	}
 
- public int getFitness() {return this.fitness;}
+	public void setHierarchical(boolean h) {
+		this.hierarchical = h;
+	}
 
- public void setHierarchical(boolean h){
-     this.hierarchical = h;
- }
+	public FitAnimal(int fitness, boolean hierarchical) {
+		this.fitness = fitness;
+		this.hierarchical = hierarchical;
+	}
 
-    public FitAnimal(int fitness, boolean hierarchical) {
-        this.fitness = fitness;
-        this.hierarchical = hierarchical;
-    }
+	public int fitter(FitAnimal compared) {
+		double margin = 0.2;
+		if (this.fitness >= (1 + margin) * compared.fitness)
+			return 1;
+		if (this.fitness <= (1 + margin) * compared.fitness && this.fitness >= (1 - margin) * compared.fitness)
+			return 0;
 
-    public int fitter (FitAnimal compared) {
-     double margin = 0.2;
-     if (this.fitness >= (1 + margin) * compared.fitness) return 1;
-     if (this.fitness <= (1 + margin) * compared.fitness && this.fitness >= (1 - margin) * compared.fitness) return 0;
+		return -1;
+	}
 
-     return -1;
- }
+	public int changeFintess(int newFitness) {
+		return this.fitness = Math.abs(newFitness);
+	}
 
- public int changeFintess(int newFitness) {
-     return this.fitness = Math.abs(newFitness);
- }
+	public boolean hierarchical() {
+		if (this.hierarchical)
+			return true;
+		return false;
+	}
 
- public boolean hierarchical () {
-     if (this.hierarchical) return true;
-     return false;
- }
+	public boolean mayBeAlpha() {
+		if (this.hierarchical())
+			return true;
+		return false;
+	}
 
- public boolean mayBeAlpha () {
-     if (this.hierarchical()) return true;
-     return false;
- }
+	@Override
+	public String toString() {
+		String h;
+		if (this.hierarchical())
+			h = "is hierarchical";
+		else
+			h = "is not hierarchical";
 
- @Override
- public String toString () {
-     String h;
-     if (this.hierarchical()) h = "is hierarchical";
-        else h = "is not hierarchical";
-
-     return "<" + " Fitness is: " + this.fitness + " | " + h + " >" ;
- }
+		return "<" + " Fitness is: " + this.fitness + " | " + h + " >";
+	}
 
 }
--- src/aufgabe5/SocialGroup.java
+++ src/aufgabe5/SocialGroup.java
@@ -1,6 +1,7 @@
 package aufgabe5;
 
 import java.util.Iterator;
+import java.util.function.Predicate;
 
 /*– iterator
 – add
@@ -10,12 +11,107 @@ import java.util.Iterator;
 – compareAll
 - toString */
 
-public class SocialGroup implements Iterable<T> {
+@SuppressWarnings("hiding")
+public class SocialGroup<FitAnimal> implements Iterable<FitAnimal> {
 
-	@Override
-	public Iterator<T> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+	private Node<FitAnimal> head, tail;
+
+	public Node<FitAnimal> getHead() {
+		return this.head;
 	}
 
+	public Node<FitAnimal> getTail() {
+		return this.tail;
+	}
+
+	@Override
+	public Iterator<FitAnimal> iterator() {
+		return new SocialGroupIterator<FitAnimal>(this);
+	}
+
+	public void add(FitAnimal a) {
+		Node<FitAnimal> node = new Node<FitAnimal>(a, null);
+		if (head == null) {
+			this.head = this.tail = node;
+		} else {
+			this.tail.setNext(node);
+			tail = node;
+		}
+	}
+
+	// TODO
+	public void move(SocialGroup<FitAnimal> source, Predicate<FitAnimal> p) {
+
+	}
+	
+	// TODO
+	public void compareAll() {
+
+	}
+	
+	// TODO
+	public boolean hierarchical() {
+		return false;
+
+	}
+
+	public Iterator<FitAnimal> alpha() {
+		return null;
+
+	}
+
+	public class SocialGroupIterator<FitAnimal> implements Iterator<FitAnimal> {
+
+		private SocialGroup<FitAnimal>.Node<FitAnimal> current;
+
+		public SocialGroupIterator(SocialGroup<FitAnimal> group) {
+			current = group.getHead();
+		}
+
+		@Override
+		public boolean hasNext() {
+			return current != null;
+		}
+
+		@Override
+		public FitAnimal next() {
+			if (!this.hasNext()) {
+				return null;
+			}
+			FitAnimal animal = current.getCurrent();
+			current = current.getNext();
+			return animal;
+		}
+		
+		// TODO
+		public void remove(FitAnimal a) {
+
+		}
+	}
+
+	public class Node<FitAnimal> {
+		private FitAnimal current;
+		private Node<FitAnimal> next;
+
+		public Node(FitAnimal current, Node<FitAnimal> next) {
+			this.current = current;
+			this.next = next;
+		}
+
+		public void setCurrent(FitAnimal current) {
+			this.current = current;
+		}
+
+		public FitAnimal getCurrent() {
+			return this.current;
+		}
+
+		public void setNext(Node<FitAnimal> next) {
+			this.next = next;
+		}
+
+		public Node<FitAnimal> getNext() {
+			return this.next;
+		}
+	}
 }
